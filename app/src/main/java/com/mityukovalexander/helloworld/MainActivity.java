c7412795d84dbdf51888765b4f3f2ce9752232f7
Alexander Mityukov <reignover.dev@gmail.com>
2019-07-09 19:37:46 +0300

    Homework Authorization

--- app/src/main/java/com/mityukovalexander/helloworld/MainActivity.java
+++ app/src/main/java/com/mityukovalexander/helloworld/MainActivity.java
@@ -4,13 +4,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,16 +17,20 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String AUTH_TOKEN = "auth_token";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView helloLoftMoney = findViewById(R.id.HelloLoftMoney);
-        helloLoftMoney.setOnClickListener(new View.OnClickListener() {
+        if (!TextUtils.isEmpty(getToken())) {
+            startBudgetActivity();
+        }
+        Button enterButton = findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, BudgetActivity.class));
+                startBudgetActivity();
             }
         });
 
@@ -44,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
                 SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("auth_token", response.body().getAuthToken());
+                editor.putString(AUTH_TOKEN, response.body().getAuthToken());
                 editor.apply();
             }
 
@@ -54,4 +56,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void startBudgetActivity() {
+        startActivity(new Intent(MainActivity.this, BudgetActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        finish();
+        overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
+    }
+
+    private String getToken() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        return sharedPreferences.getString(AUTH_TOKEN, "");
+    }
 }
